abbfea1ab8ef23c393f2ab5a9faf5e097d5626a3
Ewerton Tavares <ewerton_tavares@live.com>
2021-10-22 13:09:18 -0300

    up

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -42,7 +42,7 @@ export function TaskList() {
     <section className="task-list container">
       <header>
         <h2>Minhas tasks</h2>
-        <div className="input-group">
+        <div className="input-group"> 
           <input
             type="text"
             placeholder="Adicionar novo todo"
